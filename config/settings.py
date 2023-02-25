1cd2cfe33d4e21942852a43cbd95c5d03354089d
KimSuhong <ksum0310@naver.com>
2023-02-25 19:41:00 +0900

    커스텀 예외 핸들러 생성

--- config/settings.py
+++ config/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'config.custom_exception_handler.ExceptionMiddleware',
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
@@ -124,6 +125,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':
     'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 3,
+    'EXCEPTION_HANDLER': 'config.custom_exception_handler.handle_exception',
 }
 
 
